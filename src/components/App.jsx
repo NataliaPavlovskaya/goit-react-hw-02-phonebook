dbf8521bac92b11a315a0612c9a19c027947ab6f
NataliaPavlovskaya <91052774+NataliaPavlovskaya@users.noreply.github.com>
2022-07-06 14:31:16 +0300

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,15 +3,12 @@ import { v4 as uuidv4 } from "uuid";
 import ContactList from "./ContactList/ContactList";
 import Filter from "./Filter/Filter";
 import ContactForm from "./ContactForm/ContactForm";
+import initialContacts from "../contacts.json"
+
 
 export default class App extends Component {
   state = {
-    contacts: [
-      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-    ],
+    contacts: initialContacts,
     filter: "",
   };
 
